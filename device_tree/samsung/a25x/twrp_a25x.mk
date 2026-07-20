935438a822e7750cdf6726b44f0e70d4940375c3
TechExplorera <131724026+TechExplorera@users.noreply.github.com>
2026-07-20 17:38:34 -0600

    Rename product from omni_a25x to twrp_a25x

--- device_tree/samsung/a25x/twrp_a25x.mk
+++ device_tree/samsung/a25x/twrp_a25x.mk
@@ -16,7 +16,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, device/samsung/a25x/device.mk)
 
 PRODUCT_DEVICE := a25x
-PRODUCT_NAME := omni_a25x
+PRODUCT_NAME := twrp_a25x
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A256E
 PRODUCT_MANUFACTURER := samsung
